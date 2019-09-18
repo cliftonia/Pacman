9fac1b2277dee24d1be49c98a1cf0af155fc83c7
Clifton <clifton.baggerman@me.com>
2019-09-18 11:30:08 +1000

    formated my code using ruby conventions

--- pacman.rb
+++ pacman.rb
@@ -1,139 +1,63 @@
-require 'pry'
-
 class Pacman
-  def initialize(x, y, f)
+  def initialize(xaxis, yaxis, facing)
     @pacman = {
-      "@x" => x,
-      "@y" => y,
-      "@f" => f.upcase
+      '@xaxis' => xaxis,
+      '@yaxis' => yaxis,
+      '@facing' => facing.upcase
     }
   end
 
-  def place(x,y,f)
-    @pacman["@x"] = x
-    @pacman["@y"] = y
-    @pacman["@f"] = f
-  end 
+  def place(xaxis, yaxis, facing)
+    @pacman['@xaxis'] = xaxis
+    @pacman['@yaxis'] = yaxis
+    @pacman['@facing'] = facing.upcase
+  end
 
   def move
-    if @pacman['@f'] == "NORTH" && @pacman['@y'] < 5
-      @pacman['@y'] += 1
-    elsif @pacman['@f'] == "SOUTH" && @pacman['@y'] > 0
-       @pacman['@y'] -= 1
-    elsif @pacman['@f'] == "EAST" && @pacman['@x'] < 5
-      @pacman['@x'] += 1
-    elsif @pacman['@f'] == "WEST" && @pacman['@x'] > 0
-      @pacman['@x'] -= 1
-    else    
-      return "YOU CANT'T MOVE ANY FURTHER"
-    end 
-  end 
+    if @pacman['@facing'] == 'NORTH' && @pacman['@yaxis'] < 5
+      @pacman['@yaxis'] += 1
+    elsif @pacman['@facing'] == 'SOUTH' && (@pacman['@yaxis']).positive?
+      @pacman['@yaxis'] -= 1
+    elsif @pacman['@facing'] == 'EAST' && @pacman['@xaxis'] < 5
+      @pacman['@xaxis'] += 1
+    elsif @pacman['@facing'] == 'WEST' && (@pacman['@xaxis']).positive?
+      @pacman['@xaxis'] -= 1
+    else
+      "YOU CAN'T MOVE ANY FURTHER"
+    end
+  end
 
   def left
-    if @pacman['@f'] == 'NORTH'
-      @pacman['@f'] = "WEST"
-    elsif @pacman['@f'] == "WEST"
-      @pacman['@f'] = "SOUTH"
-    elsif @pacman['@f'] == "SOUTH"
-      @pacman['@f'] = "EAST"
-    elsif @pacman['@f'] == "EAST"
-      @pacman['@f'] = "NORTH"
+    if @pacman['@facing'] == 'NORTH'
+      @pacman['@facing'] = 'WEST'
+    elsif @pacman['@facing'] == 'WEST'
+      @pacman['@facing'] = 'SOUTH'
+    elsif @pacman['@facing'] == 'SOUTH'
+      @pacman['@facing'] = 'EAST'
+    elsif @pacman['@facing'] == 'EAST'
+      @pacman['@facing'] = 'NORTH'
     end
-  end 
+  end
 
   def right
-    if @pacman['@f'] == 'NORTH'
-      @pacman['@f'] = "EAST"
-    elsif @pacman['@f'] == "EAST"
-      @pacman['@f'] = "SOUTH"
-    elsif @pacman['@f'] == "SOUTH"
-      @pacman['@f'] = "WEST"
-    elsif @pacman['@f'] == "WEST"
-      @pacman['@f'] = "NORTH"
+    if @pacman['@facing'] == 'NORTH'
+      @pacman['@facing'] = 'EAST'
+    elsif @pacman['@facing'] == 'EAST'
+      @pacman['@facing'] = 'SOUTH'
+    elsif @pacman['@facing'] == 'SOUTH'
+      @pacman['@facing'] = 'WEST'
+    elsif @pacman['@facing'] == 'WEST'
+      @pacman['@facing'] = 'NORTH'
     end
-  end 
+  end
 
   def report
-    value = []
-    @pacman.each do |k, v|
-       value.push(v)
-    end 
+    board_position = []
+    @pacman.each do |_k, v|
+      board_position.push(v)
+    end
 
-    return value.join(', ')
-  end 
-
+    board_position.join(', ')
+  end
 end
 
-# puts "WELCOME TO PACMAN"
-# puts "PACMAN RESIDES ON A 5 X 5 BOARD"
-# puts "HE CAN FACE NORTH, SOUTH, EAST AND WEST"
-
-# loop do
-#   puts "TO BEGIN PLESE PROVIDE AN X COORDINATE BETWEEN 0 - 5"
-#   @x = gets.chomp.to_i
-
-#   if @x.between?(0,5)
-#     break
-#   else  
-#     puts "NUMBER INVALID"
-#   end 
-
-# end 
-
-# loop do
-#   puts "PLEASE PROVIDE AN Y COORDINATE BETWEEN 0 - 5"
-#   @y = gets.chomp.to_i
-
-#   if @y.between?(0,5)
-#     break
-#   else  
-#     puts "NUMBER INVALID"
-#   end 
-
-# end 
-
-# loop do 
-#   puts "PLEASE PROVIDE WHICH DIRECTION YOU WANT PACMAN TO FACE"
-#   puts 'NORTH'
-#   puts 'SOUTH'
-#   puts 'EAST'
-#   puts 'WEST'
-
-#   @f = gets.chomp.upcase
-
-#   if @f == "NORTH" || @f == "SOUTH" || @f == "EAST" || @f == "WEST"
-#     break
-#   else  
-#     puts "INVALID DIRECTION"
-#   end
-  
-# end 
-
-# pacman = Pacman.new(@x, @y, @f)
-
-# puts "TO BEGIN PLEASE TYPE: PLACE"
-# puts "MOVE PACMAN BY TYPING MOVE"
-# puts "YOU CAN CHANGE DIRECTION BY EITHER TYPING: LEFT OR RIGHT"
-# puts "TO SEE WHERE YOU ARE ON THE BOARD TYPE: REPORT"
-# puts "TO QUIT TYPE: EXIT"
-
-# loop do
-#   command = gets.chomp.upcase
-
-#   if command == "PLACE"
-#     puts pacman.report
-#   elsif command == 'MOVE'
-#     puts "PACMAN MOVED TO POSTION: #{pacman.move}"
-#   elsif command == 'LEFT'
-#     puts "PACMAN IS NOW FACING #{pacman.left}"
-#   elsif command == 'RIGHT'
-#     puts "PACMAN IS NOW FACING #{pacman.right}"
-#   elsif command == 'REPORT'
-#     puts pacman.report
-#   elsif command == 'EXIT'
-#     break
-#   end 
-
-#  end
-
-binding.pry
