06eeef97653dad1fbb682dd983d9c4d8753438b4
Clifton <clifton.baggerman@me.com>
2019-09-18 13:33:21 +1000

    fixed spelling mistakes

--- main.rb
+++ main.rb
@@ -1,5 +1,3 @@
-require 'pry'
-
 require_relative 'pacman'
 require_relative 'pacman_methods'
 
@@ -7,7 +5,6 @@ pacman = Pacman.new(0, 0, 'SOUTH')
 
 puts 'WELCOME TO PACMAN'
 puts 'PACMAN RESIDES ON A 5 X 5 BOARD'
-puts 'HE CAN FACE NORTH, SOUTH, EAST AND WEST'
 
 puts 'TO BEGIN PLEASE TYPE PLACE'
 
@@ -20,9 +17,9 @@ loop do
     facing_direction
     pacman.place(@xaxis, @yaxis, @facing)
     puts pacman.report
-    puts 'YOUE CAN MOVE PACMAN BY TYPING MOVE'
-    puts 'YOU CAN CHANGE DIRECTION BY TYPING LEFT OR RIGHT'
-    puts 'TO SEE WHERE YOU ARE ON THE BOARD TYPE: REPORT'
+    puts 'PACMAN CAN MOVE BY TYPING: MOVE'
+    puts 'YOU CAN CHANGE DIRECTION BY TYPING: LEFT OR RIGHT'
+    puts 'TO SEE WHERE PACMAN IS TYPE: REPORT'
     puts 'TO QUIT TYPE: EXIT'
   elsif command == 'MOVE'
     puts "PACMAN MOVED TO POSTION: #{pacman.move}"
